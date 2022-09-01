7499ecc2db91f5b930d294107e6f1466f5c1db68
javito96 <javier.cornejo.niederle@gmail.com>
2022-09-01 14:23:37 -0300

    por entregar

--- client/src/component/CreatePoke.jsx
+++ client/src/component/CreatePoke.jsx
@@ -1,56 +1,99 @@
 import React, { useState, useEffect } from "react";
-import { Link, useHistory } from 'react-router-dom';
-import { postPokemons, getTypes } from '../action/index'
+import { Link, useHistory } from "react-router-dom";
+import { postPokemons, getTypes } from "../action/index";
 import { useDispatch, useSelector } from "react-redux";
-import './CreatePoke.css'
+import "./CreatePoke.css";
 
 function validate(input) {
-    const imgValidate = /(https?:\/\/.*\.(?:png|jpg))/;
-        let testName = /^[A-Z][a-z][^$()!¡@#/=¿{}?*%&|<>#]*$/;
-    let errors = {};
+  const imgValidate = /(https?:\/\/.*\.(?:png|jpg))/;
+  let testName = /^[A-Z][a-z][^$()!¡@#/=¿{}?*%&|<>#]*$/;
+  let errors = {};
 
-    if(!input.name) {
-      errors.name = "Name is Required";
-    }
-    else if (!testName.test(input.name)) {
-        errors.name = 'Start the title with capital letter. Only characters "":.,_- are accepted'
-    }
-    else if (!input.hp) {
-        errors.hp = 'Please range of motion'
-      }
-    else if (!input.attack) {
-        errors.attack = 'Please range of motion'
-      }
-    else if (!input.defense) {
-        errors.defense = 'Please range of motion'
-      }
-    else if (!input.speed) {
-        errors.speed = 'Please range of motion'
-      }
-    else if (!input.height) {
-        errors.height = 'Please range of motion'
-      }
-    else if (!input.weight) {
-        errors.weight = 'Please range of motion'
-      }
-      
-        else if (!input.img || !imgValidate.test(input.img)) {
-            errors.img = 'Please insert an image type URL'
-        }  
-    else if (!input.type) {
-        errors.type = 'Please select Type'
-      }
-    return errors;
+  if (!input.name) {
+    errors.name = "Name is Required";
+  }
+  if (!testName.test(input.name)) {
+    errors.name =
+      'Start the title with capital letter. Only characters "":.,_- are accepted';
+  }
+  if (!input.hp) {
+    errors.hp = "Please range of motion";
+  }
+  if (!input.attack) {
+    errors.attack = "Please range of motion";
+  }
+  if (!input.defense) {
+    errors.defense = "Please range of motion";
+  }
+  if (!input.speed) {
+    errors.speed = "Please range of motion";
+  }
+  if (!input.height) {
+    errors.height = "Please range of motion";
+  }
+  if (!input.weight) {
+    errors.weight = "Please range of motion";
   }
 
+  if (!input.img || !imgValidate.test(input.img)) {
+    errors.img = "Please insert an image type URL";
+  }
+  //   if (!input.type) {
+  //     errors.type = "Please select Type";
+  //   }
+  return errors;
+}
 
 export default function PokeCreate() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const types = useSelector((state) => state.types);
-    const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const types = useSelector((state) => state.types);
+  const [errors, setErrors] = useState({});
 
-    const [input, setInput] = useState({
+  const [input, setInput] = useState({
+    name: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    height: "",
+    weight: "",
+    image: "",
+    types: [],
+  });
+
+  function handleChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+    console.log(input);
+  }
+  function handleSelect(e) {
+    setInput({
+      ...input,
+      types: [...input.types, e.target.value],
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault(); // evitamos volver a cargar la pagina evento por default
+    console.log(input);
+    if (Object.values(errors).length > 0) {
+      alert("Please fill in all the fields");
+      console.log("handleSubmit ", { errors });
+      // console.log('handleSubmit ', post.lifeMin);
+      // console.log('handleSubmit ', post.lifeMax);
+    } else {
+      dispatch(postPokemons(input));
+      alert(`Pokemon created successfully!`);
+      setInput({
         name: "",
         hp: "",
         attack: "",
@@ -58,219 +101,169 @@ export default function PokeCreate() {
         speed: "",
         height: "",
         weight: "",
-        image: "",
         types: [],
-    });
-
-    
-
-    function handleChange(e) {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
-        setErrors(validate({
-            ...input,
-            [e.target.name]: e.target.value
-        }))
-        console.log(input)
+      });
+      history.push("/home");
     }
-    function handleSelect(e) {
-        setInput({
-            ...input,
-            types: [...input.types, e.target.value]
-        })
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault(); // evitamos volver a cargar la pagina evento por default
-        console.log(input)
-        dispatch(postPokemons(input));
-        alert(`Pokemon created successfully!`);
-        setInput({
-            name: "",
-            hp: "",
-            attack: "",
-            defense: "",
-            speed: "",
-            height: "",
-            weight: "",
-            image: "",
-            types: []
-        });
-        history.push("/home");
-    }
-
-    function handleDelete(el){
+  }
+  function handleDelete(el) {
     setInput({
-        ...input,
-        types: input.types.filter(di => di !== el)
-    })
-    }
+      ...input,
+      types: input.types.filter((di) => di !== el),
+    });
+  }
 
+  useEffect(() => {
+    dispatch(getTypes());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getTypes())
-    }, [dispatch]);
-
-    return (
-        <div className="fondo">
-            <div className="contaitner">
-                <div>
-                    <Link  to='/home'>
-                <button className="button" id="button">
-                        Back home
-                </button>
-                        </Link>
-                </div>
-                
-                <div>
-                    
-            <h1 className="title">Create your Poke</h1>
-
-                </div>
-            <form  onSubmit={(e) => handleSubmit(e)}>
-                <div>
-                    <label>Name</label>
-                    <input
-                        type='text'
-                        value={input.name}
-                        name='name'
-                        onChange={(e) => handleChange(e)}
-                        />
-                        {errors.name && <p >{errors.name}</p>}
-                </div>
-
-                <div>
-                    <label>Life</label>
-                    <input
-                        type='range'
-                        value={input.hp}
-                        name='hp'
-                        min="0"
-                        max="100"
-                        onChange={(e) => handleChange(e)}
-                        />
-                         {errors.hp && <p>{errors.hp}</p>}
-                </div>
-
-
-                <div>
-                    <label>Attack:</label>
-                     <input
-                       type="range"
-                       value={input.attack}
-                       name="attack"
-                       min="0"
-                       max="100"
-                       onChange={(e) => handleChange(e)}
-                     />
-                      {errors.attack && <p>{errors.attack}</p>}
-                    
-                 </div>
-
-            
-
-                <div>
-                    <label>Defense</label>
-                    <input
-                        type='range'
-                        value={input.defense}
-                        name='defense'
-                        min="0"
-                        max="100"
-                        onChange={(e) => handleChange(e)}
-                        />
-                        {errors.defense && <p>{errors.defense}</p>}
-                </div>
-
-                <div>
-                    <label>Speed :</label>
-                    <input
-                        type="range"
-                        value={input.speed}
-                        name="speed"
-                        min="0"
-                        max="100"
-                        onChange={(e) => handleChange(e)}
-                        />
-                        {errors.speed && <p>{errors.speed}</p>}
-                </div>
-
-                <div>
-                    <label>Heigth</label>
-                    <input
-                        type='range'
-                        value={input.height}
-                        name='height'
-                        onChange={(e) => handleChange(e)}
-                        />
-                         {errors.height && <p>{errors.height}</p>}
-                </div>
-
-                <div>
-                      <label>Weight:</label>
-                      <input
-                        type="range"
-                        value={input.weight}
-                        name="weight"
-                        onChange={(e) => handleChange(e)}
-                      />
-                        {errors.weight && <p>{errors.weight}</p>}                      
-                    
-                 </div>
-
-                <div>
-                    <label>Image(URL)</label>
-                    <input
-                        type='text'
-                        value={input.img}
-                        name='img'
-                        onChange={(e) => handleChange(e)}
-                        />
-                         {errors.img && <p>{errors.img}</p>}
-                </div>
-                 
-
-                 <div>
-                              
-                <select onChange={(e) => handleSelect(e)}>
-                    {types.map((type, t ) => (
-                        <option  key={t} value={type.name}>{type.name}</option>
-                        ))}
-                </select>
-
-                        </div>                   
-
-
-
-                <ul><li
-                className="li"
-                >{input.types.map(el => el + ', ')} </li></ul>
-
-                <div>
-            <button className="button" id="button" type="submit" disabled={!input.name} >Create Pokemon</button>
-
-                </div>
-                      
-
-
-
-
-
-            
-            </form>
-            
-            {input.types.map(el=>
-            <div className="types1">
-                <p>{el}</p>
-                <button className="buttonx" onClick={() => handleDelete(el)}> x </button>
-            </div>              
-                        
-                        )}
-            </div>
+  return (
+    <div className="fondo">
+      <div className="contaitner">
+        <div>
+          <Link to="/home">
+            <button className="button" id="button">
+              Back home
+            </button>
+          </Link>
         </div>
-    )
 
+        <div>
+          <h1 className="title">Create your Poke</h1>
+        </div>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              value={input.name}
+              name="name"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.name && <p>{errors.name}</p>}
+          </div>
 
+          <div>
+            <label>Life</label>
+            <input
+              type="range"
+              value={input.hp}
+              name="hp"
+              min="0"
+              max="100"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.hp && <p>{errors.hp}</p>}
+          </div>
+
+          <div>
+            <label>Attack:</label>
+            <input
+              type="range"
+              value={input.attack}
+              name="attack"
+              min="0"
+              max="100"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.attack && <p>{errors.attack}</p>}
+          </div>
+
+          <div>
+            <label>Defense</label>
+            <input
+              type="range"
+              value={input.defense}
+              name="defense"
+              min="0"
+              max="100"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.defense && <p>{errors.defense}</p>}
+          </div>
+
+          <div>
+            <label>Speed :</label>
+            <input
+              type="range"
+              value={input.speed}
+              name="speed"
+              min="0"
+              max="100"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.speed && <p>{errors.speed}</p>}
+          </div>
+
+          <div>
+            <label>Heigth</label>
+            <input
+              type="range"
+              value={input.height}
+              name="height"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.height && <p>{errors.height}</p>}
+          </div>
+
+          <div>
+            <label>Weight:</label>
+            <input
+              type="range"
+              value={input.weight}
+              name="weight"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.weight && <p>{errors.weight}</p>}
+          </div>
+
+          <div>
+            <label>Image(URL)</label>
+            <input
+              type="text"
+              value={input.img}
+              name="img"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.img && <p>{errors.img}</p>}
+          </div>
+
+          <div>
+            <select onChange={(e) => handleSelect(e)}>
+              {types.map((type) => (
+                <option key={type.name} value={type.name}>
+                  {type.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <ul>
+            <li className="li">{input.types.map((el) => el + ", ")} </li>
+          </ul>
+
+          <div>
+            <button
+              className="button"
+              id="button"
+              type="submit"
+              disabled={!input.name}
+            >
+              Create Pokemon
+            </button>
+          </div>
+        </form>
+
+        {input.types.map((el) => (
+          <div className="types1">
+            <p>{el}</p>
+            <button className="buttonx" onClick={() => handleDelete(el)}>
+              {" "}
+              x{" "}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,176 +1,174 @@
 import React from "react";
-import { useState, useEffect  } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { getPoke, fulterPokemonsByTypes, filterDB, orderByName, orderByAttack } from "../action";
-import Card from './Card'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getPoke,
+  fulterPokemonsByTypes,
+  filterDB,
+  orderByName,
+  orderByAttack,
+} from "../action";
+import Card from "./Card";
 import { Link } from "react-router-dom";
 import Paginado from "./Paginado";
 import SearchPoke from "./SearchPoke";
-import './Home.css'
+import "./Home.css";
 import Loading from "./Loading";
 
-export default function Home(){
-    const dispatch = useDispatch();
-    const allPoke = useSelector((state) => state.pokemons)
+export default function Home() {
+  const dispatch = useDispatch();
+  const allPoke = useSelector((state) => state.pokemons);
 
+  const [currentPage, setCurrentPage] = useState(1); //estado local con primer pag que renderiza
+  const [pokesPerPage] = useState(12); //en la primer pag trae 9 recetas
+  const indexOfLastPokes = currentPage * pokesPerPage; //numero del ultimo indice multiplicado pag, ej recetas 9 pag nro3 = 27
+  const indexOfFirstPokes = indexOfLastPokes - pokesPerPage; //indice de ultima receta - recetas por pag igual a indice de primer receta
+  const currentPokes = allPoke.slice(indexOfFirstPokes, indexOfLastPokes); //slice toma una porcion de lo que yo le paso por parametro
+  const [Ordenado, setOrder] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const [currentPage, setCurrentPage ] = useState(1)//estado local con primer pag que renderiza
-    const [pokesPerPage] = useState(12)//en la primer pag trae 9 recetas
-    const indexOfLastPokes = currentPage * pokesPerPage//numero del ultimo indice multiplicado pag, ej recetas 9 pag nro3 = 27
-    const indexOfFirstPokes = indexOfLastPokes - pokesPerPage//indice de ultima receta - recetas por pag igual a indice de primer receta
-    const currentPokes = allPoke.slice(indexOfFirstPokes, indexOfLastPokes) //slice toma una porcion de lo que yo le paso por parametro
-    const [Ordenado, setOrder] = useState('')
+  const paginado = (pageNumbers) => {
+    setCurrentPage(pageNumbers);
+  }; // ayuda renderizado
 
-    const paginado = (pageNumbers) => {
-        setCurrentPage(pageNumbers)
-    }// ayuda renderizado
-    
-    useEffect(() => {
-        dispatch(getPoke())
-},[dispatch])
+  useEffect(() => {
+    setLoading(true);
+    dispatch(getPoke());
+    setLoading(false);
+  }, [dispatch]);
 
-
-function handleClick(e){
+  function handleClick(e) {
     e.preventDefault();
-    dispatch(getPoke())
-}
-
-
-
-function handleFilterByType(e){
-    dispatch(fulterPokemonsByTypes(e.target.value))
-    setCurrentPage(1)
-}
-
-function hadleFilterDB(e){
-    dispatch(filterDB(e.target.value))
-}
-
-function handleSort(e){
-    e.preventDefault()
-    dispatch(orderByName(e.target.value))
-    setCurrentPage(1)
-    setOrder(`Ordenado ${e.target.value}`)
+    dispatch(getPoke());
   }
 
-  function handleByAttack(e){
-    e.preventDefault()
-    dispatch(orderByAttack(e.target.value))
-    setCurrentPage(1)
-    setOrder(`Ordenado ${e.target.value}`)
+  function handleFilterByType(e) {
+    dispatch(fulterPokemonsByTypes(e.target.value));
+    setCurrentPage(1);
   }
 
+  function hadleFilterDB(e) {
+    dispatch(filterDB(e.target.value));
+  }
 
+  function handleSort(e) {
+    e.preventDefault();
+    dispatch(orderByName(e.target.value));
+    setCurrentPage(1);
+    setOrder(`Ordenado ${e.target.value}`);
+  }
 
-return (
+  function handleByAttack(e) {
+    e.preventDefault();
+    dispatch(orderByAttack(e.target.value));
+    setCurrentPage(1);
+    setOrder(`Ordenado ${e.target.value}`);
+  }
+
+  return (
     <div className="back1">
-        <h1 className="title">POKLEJO</h1>
-        <div>
-         <Link to='/poke'>
-            <button id="button" >CREATE NEW POKEMON</button></Link>
-         
-         <button  id="button" onClick={e=> {handleClick(e)}}>LOAD POKEMONS</button>
+      {loading && <Loading />}
+      <h1 className="title">POKLEJO</h1>
+      <div>
+        <Link to="/poke">
+          <button id="button">CREATE NEW POKEMON</button>
+        </Link>
 
-        </div>
+        <button
+          id="button"
+          onClick={(e) => {
+            handleClick(e);
+          }}
+        >
+          LOAD POKEMONS
+        </button>
+      </div>
 
-        <h5></h5>
+      <h5></h5>
 
+      <select onChange={(e) => handleSort(e)}>
+        <option disabled selected defaultValue>
+          Alphabetical order
+        </option>
+        <option value="asc">A-Z</option>
+        <option value="desc">Z-A</option>
+      </select>
 
-         <select  onChange={(e)=>handleSort(e)} >
-                 <option disabled selected defaultValue>
-                     Alphabetical order
-                 </option>
-                 <option value="asc">A-Z</option>
-                 <option value="desc">Z-A</option>
-         </select>
+      <select onChange={(e) => handleByAttack(e)}>
+        <option disabled selected defaultValue>
+          Attack order
+        </option>
+        <option value="max">MAX</option>
+        <option value="min">MIN</option>
+      </select>
 
-         <select  onChange={(e)=>handleByAttack(e)} >
-                 <option disabled selected defaultValue>
-                 Attack  order 
-                 </option>
-                 <option value="max">MAX</option>
-                 <option value="min">MIN</option>
-         </select>
+      <select onChange={(e) => hadleFilterDB(e)}>
+        <option disabled selected defaultValue>
+          Filter new/existing{" "}
+        </option>
+        <option value="all">All</option>
+        <option value="created">Created</option>
+        <option value="ex">Existing</option>
+      </select>
 
+      <select onChange={(e) => handleFilterByType(e)}>
+        <option disabled selected defaultValue>
+          Filter by types
+        </option>
+        <option value="normal">Normal</option>
+        <option value="fighting">Fighting</option>
+        <option value="flying">Flying</option>
+        <option value="poison">Poison</option>
+        <option value="ground">Ground</option>
+        <option value="rock">Rock</option>
+        <option value="bug">Bug</option>
+        <option value="ghost">Ghost</option>
+        <option value="steel">Steel</option>
+        <option value="fire">Fire</option>
+        <option value="water">Water</option>
+        <option value="grass">Grass</option>
+        <option value="electric">Electric</option>
+        <option value="psychic">Psychic</option>
+        <option value="ice">Ice</option>
+        <option value="dragon">Dragon</option>
+        <option value="dark">Dark</option>
+        <option value="fairy">Fairy</option>
+        <option value="unknown">Unknown</option>
+        <option value="shadow">Shadow</option>
+      </select>
 
-         <select onChange={(e) => hadleFilterDB(e)}>
-         <option disabled selected defaultValue>
-                    Filter new/existing </option>
-              <option value="all">All</option>
-              <option value="created">Created</option>
-              <option value="ex">Existing</option>
-            </select>
+      <div>
+        {" "}
+        <SearchPoke />{" "}
+      </div>
 
-            
+      <div className="cards">
+        {currentPokes?.map((c) => {
+          return (
+            <div key={c.id}>
+              <div>
+                <Link className="subrayado" to={"/detail/" + c.id}>
+                  <Card
+                    id={c.id}
+                    key={c.id}
+                    name={c.name}
+                    types={c.types}
+                    img={c.img}
+                  />
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-
-
-         <select onChange={(e)=>handleFilterByType(e)}>
-                <option disabled selected defaultValue>Filter by types</option>
-                <option value="normal">Normal</option>
-                <option value="fighting">Fighting</option>
-                <option value="flying">Flying</option>
-                <option value="poison">Poison</option>
-                <option value="ground">Ground</option>
-                <option value="rock">Rock</option>
-                <option value="bug">Bug</option>
-                <option value="ghost">Ghost</option>
-                <option value="steel">Steel</option>
-                <option value="fire">Fire</option>
-                <option value="water">Water</option>
-                <option value="grass">Grass</option>
-                <option value="electric">Electric</option>
-                <option value="psychic">Psychic</option>
-                <option value="ice">Ice</option>
-                <option value="dragon">Dragon</option>
-                <option value="dark">Dark</option>
-                <option value="fairy">Fairy</option>
-                <option value="unknown">Unknown</option>
-                <option value="shadow">Shadow</option>
-            </select>
-
-            <div> <SearchPoke/> </div>
-
-
-<div className='cards'>
-
-{
-    currentPokes?.map((c) => {
-            return(
-                <div key={c.id}> 
-                <div>
-                    <Link className="subrayado" to={'/detail/' + c.id}>
-                        <Card 
-                        id={c.id}
-                        key={c.id}  
-                        name={c.name} 
-                        types={c.types} 
-                        img={c.img} />
-                    </Link>
-                </div>
-                </div>
-            )
-        })
-      
-} 
-</div>
-
-
-    {/* <Loading/> */}
-
-
-
-
-<div>
-    <Paginado
-    pokesPerPage={pokesPerPage}
-    allPoke={allPoke.length-1}
-    paginado={paginado}    
-    />
-</div>
-
-
+      <div>
+        <Paginado
+          pokesPerPage={pokesPerPage}
+          allPoke={allPoke.length - 1}
+          paginado={paginado}
+        />
+      </div>
     </div>
-)
-
+  );
 }
